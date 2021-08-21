e878ccc723eead4ad39456347a691596a92c7720
주진성 <jjsair0412@naver.com>
2021-08-21 19:32:10 +0900

    메시지 관리, 국제화 설정 - 전체수정

--- src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -1,7 +1,5 @@
 package hello.itemservice.message;
 
-import org.assertj.core.api.AbstractStringAssert;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
